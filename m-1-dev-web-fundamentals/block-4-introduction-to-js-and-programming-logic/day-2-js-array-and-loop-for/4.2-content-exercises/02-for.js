0374b132d3229a868e0ffef83bf6f220f90dd7bd
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-08-20 00:52:38 -0300

    content-exercises

---
+++ m-1-dev-web-fundamentals/block-4-introduction-to-js-and-programming-logic/day-2-js-array-and-loop-for/4.2-content-exercises/02-for.js
@@ -0,0 +1,5 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let i = 0; i < 4; i++) {
+    console.log(groceryList[i])
+}
---
+++ m-1-dev-web-fundamentals/block-4-introduction-to-js-and-programming-logic/day-2-js-array-and-loop-for/4.2-content-exercises/03-for-of.js
@@ -0,0 +1,31 @@
+//Exercise 1
+let numeros = [1,2,3,4,5];
+for(let numero of numeros) {
+  console.log(numero);
+}
+
+console.log(" ");
+//----------------------------
+//Exercise 2
+let word = 'Hello';
+for (let letter of word) {
+    console.log(letter);
+}
+
+console.log(" ");
+//----------------------------
+//Exercise 3
+let arrOfNumbers = [10, 20, 30];
+for (let sum of arrOfNumbers) {
+  sum += 1;
+  console.log(sum);
+}
+
+console.log(" ");
+//----------------------------
+//Exercise 4
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let friends of names) {
+  console.log(friends);
+}
+
