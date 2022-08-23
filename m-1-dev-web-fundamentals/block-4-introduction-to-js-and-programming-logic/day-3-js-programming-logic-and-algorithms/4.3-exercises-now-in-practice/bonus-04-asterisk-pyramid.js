462ccdfa84c3546787648a3ca7d53b9b64c4daad
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-08-23 06:40:21 -0300

    Update exercises bonus

---
+++ m-1-dev-web-fundamentals/block-4-introduction-to-js-and-programming-logic/day-3-js-programming-logic-and-algorithms/4.3-exercises-now-in-practice/bonus-04-asterisk-pyramid.js
@@ -0,0 +1,20 @@
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex++) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex++) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
+      inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight++;
+  controlLeft--;
+};
