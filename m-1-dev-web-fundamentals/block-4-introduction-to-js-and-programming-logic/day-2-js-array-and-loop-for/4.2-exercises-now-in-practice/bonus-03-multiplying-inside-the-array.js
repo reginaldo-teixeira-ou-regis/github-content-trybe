6822ca4935774f815c20ea1d1aec012051ca714b
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-08-24 03:23:27 -0300

    Update exercises

--- m-1-dev-web-fundamentals/block-4-introduction-to-js-and-programming-logic/day-2-js-array-and-loop-for/4.2-exercises-now-in-practice/bonus-03-multiplying-inside-the-array.js
+++ m-1-dev-web-fundamentals/block-4-introduction-to-js-and-programming-logic/day-2-js-array-and-loop-for/4.2-exercises-now-in-practice/bonus-03-multiplying-inside-the-array.js
@@ -1,3 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-//[45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+let newArray = [];
 
+for (let index = 0; index < numbers.length; index += 1) {
+  if (index + 1 < numbers.length) {
+    newArray.push(numbers[index] * numbers[index + 1]);
+  } else {
+    newArray.push(numbers[index] * 2);
+  }
+}
+
+console.log(newArray);
