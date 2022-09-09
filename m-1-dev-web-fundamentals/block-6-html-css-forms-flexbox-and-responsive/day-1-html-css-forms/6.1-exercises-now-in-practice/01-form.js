cbe00ba390ae332685784afeb15118b67f8a891a
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-09-09 13:05:09 -0300

    Update exercises bonus day 6.1

--- m-1-dev-web-fundamentals/block-6-html-css-forms-flexbox-and-responsive/day-1-html-css-forms/6.1-exercises-now-in-practice/01-form.js
+++ m-1-dev-web-fundamentals/block-6-html-css-forms-flexbox-and-responsive/day-1-html-css-forms/6.1-exercises-now-in-practice/01-form.js
@@ -1,22 +1,32 @@
-const buttonSubmit = document.querySelector('#submit-btn');
-const toAgree2 = document.getElementById('to-agree-2');
+const buttonSubmit = document.querySelector("#submit-btn");
+const toAgree2 = document.getElementById("to-agree-2");
+const fullName = document.getElementById("full-name");
+const date = document.getElementById("date");
 
-function submitDoesNothing (event) {
-    if(toAgree2 !== toAgree2.checked) {
-        event.preventDefault();
-        alert('Dados Inválidos');
-        return;
-    } else {
-        buttonSubmit.disabled = toAgree2.checked;
-        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
-    }
-    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
-}
-buttonSubmit.addEventListener('click', submitDoesNothing);
-
-/* function enableSubmit() {
-    const submitBtn = document.querySelector('#submit-btn');
-    const agreement = document.querySelector('#agreement');
-    submitBtn.disabled = !agreement.checked;
+function submitDoesNothing(event) {
+  if (toAgree2.checked && fullName === required && date === required) {
+    alert("Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.");
+  } else {
+    event.preventDefault();
+    alert("Dados Inválidos");
+    buttonSubmit = disabled;
   }
- */
+}
+buttonSubmit.addEventListener("click", submitDoesNothing);
+
+/* function textInputValidation() { //limitar caracteres pelo js
+  const email = document.querySelector("#e-mail").value.length;
+  const invalidEmail = email < 10 || email > 50;
+
+  const name = document.querySelector("#full-name").value.length;
+  const invalidName = name < 10 || name > 40;
+
+  const reason = document.querySelector("#type-answer").value.length;
+  const invalidReason = reason > 500;
+
+  if (invalidEmail || invalidName || invalidReason) {
+    return false;
+  } else {
+    return true;
+  }
+} */
