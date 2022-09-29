7f95c4d4b0ab4d9c77df77a6a7a3ba01c32074a2
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-09-29 04:38:30 -0300

    Exercises of content finished 8.5

--- m-1-dev-web-fundamentals/block-8-higher-order-functions-of-js-es6/day-5-js-es6-spread-operator-rest-parameter-destructuring-and-more/8.5-content-exercises/01-spread-operator.js
+++ m-1-dev-web-fundamentals/block-8-higher-order-functions-of-js-es6/day-5-js-es6-spread-operator-rest-parameter-destructuring-and-more/8.5-content-exercises/01-spread-operator.js
@@ -13,63 +13,106 @@ const fruitSalad = (fruit, additional) => dessert = ['abacate', ...fruit, 'melan
 console.log(fruitSalad(specialFruit, additionalItens));
 console.log('====================================================');
 
-const calculateImc = (weight, height) => (weight / (height * height)).toFixed(2);
+const calculateImc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
 
-function ImcAbove20Years(weight, height) {
-    if(calculateImc(weight, height) <= 18.5) {
+function ImcAbove20Years(peso, altura) {
+    if(calculateImc(peso, altura) <= 18.5) {
         return 'Abaixo do peso';
-    } else if (calculateImc(weight, height) <= 24.9) {
+    } else if (calculateImc(peso, altura) <= 24.9) {
         return 'Peso normal';
-    } else if (calculateImc(weight, height) <= 29.9) {
+    } else if (calculateImc(peso, altura) <= 29.9) {
         return 'Pré-obesidade';
-    } else if (calculateImc(weight, height) <= 34.9) {
+    } else if (calculateImc(peso, altura) <= 34.9) {
         return 'Obesidade Grau 1';
-    } else if (calculateImc(weight, height) <= 39.9) {
+    } else if (calculateImc(peso, altura) <= 39.9) {
         return 'Obesidade Grau 2';
+    } else if (calculateImc(peso, altura) > 40) {
+        return 'Obesidade Grau 3';
     } else {
-        return 'Obesidade Grau 3'
+        return null;
     }
 }
 
 const patientData01 = {
-    nome1: 'Rogerio de Oliveira',
-    idade1: 36,
+    nome: 'Rogerio de Oliveira',
+    idade: 36,
     altura1: 1.75,
     peso1: 85,
-    imc1: 0,
-    resultadoImc1: '',
+    imc: 0,
+    resultadoImc: ImcAbove20Years(),
 };
 
-patientData01.imc1 = calculateImc(patientData01.peso1, patientData01.altura1);
-patientData01.resultadoImc1 = ImcAbove20Years(patientData01.peso1, patientData01.altura1);
+const { peso1, altura1 } = patientData01
+patientData01.imc = calculateImc(peso1, altura1);
+patientData01.resultadoImc = ImcAbove20Years(peso1, altura1);
 
 const patientData03 = {
-    nome3: 'Bob Nelson',
-    idade3: 57,
+    nome: 'Bob Nelson',
+    idade: 57,
     altura3: 1.73,
     peso3: 110,
-    imc3: 0,
-    resultadoImc3: ImcAbove20Years(),
+    imc: 0,
+    resultadoImc: ImcAbove20Years(),
 };
 
-patientData03.imc3 = calculateImc(patientData03.peso3, patientData03.altura3);
-patientData03.resultadoImc3 = ImcAbove20Years(patientData03.peso3, patientData03.altura3);
+const { peso3, altura3 } = patientData03
+patientData03.imc = calculateImc(peso3, altura3);
+patientData03.resultadoImc = ImcAbove20Years(peso3, altura3);
 
-const patientDataUpdate = (patient1, patient2) => ({
+const patientData04 = {
+    nome: 'Mel Lisboa',
+    idade: 14,
+    altura4: 1.43,
+    peso4: 50,
+    imc: 0,
+    resultadoImc: ImcAbove20Years(),
+};
+
+const { peso4, altura4 } = patientData04
+patientData04.imc = calculateImc(peso4, altura4);
+patientData04.resultadoImc = ImcAbove20Years(peso4, altura4);
+
+const patientData05 = {
+    nome: 'Julio Manson',
+    idade: 20,
+    altura5: 1.70,
+    peso5: 53,
+    imc: 0,
+    resultadoImc: ImcAbove20Years(),
+};
+
+const { peso5, altura5 } = patientData05
+patientData05.imc = calculateImc(peso5, altura5);
+patientData05.resultadoImc = ImcAbove20Years(peso5, altura5);
+
+const patientData06 = {
+    nome: 'Jorge Dickson',
+    idade: 20,
+    altura6: 1.80,
+    peso6: 150,
+    imc: 0,
+    resultadoImc: ImcAbove20Years(),
+};
+
+const { peso6, altura6 } = patientData06
+patientData06.imc = calculateImc(peso6, altura6);
+patientData06.resultadoImc = ImcAbove20Years(peso6, altura6);
+
+const patientDataUpdate = () => ({
     ...patientData01,
     patientData02: {
-        nome2: 'Ana Julia',
-        idade2: 29,
-        altura2: 1.68,
-        peso2: 98,
-        imc2: 0,
-        resultadoImc2: ImcAbove20Years(),
+        nome: 'Ana Julia',
+        idade: 29,
+        altura: 1.68,
+        peso: 98,
+        imc: 0,
+        resultadoImc: ImcAbove20Years(),
     },
     ...patientData03,
 });
 
-const {patientData02, patientData02: {peso2, altura2}} = patientDataUpdate();
-patientData02.imc2 = calculateImc(peso2, altura2);
-patientData02.resultadoImc2 = ImcAbove20Years(peso2, altura2);
+const {patientData02, patientData02: {peso, altura}} = patientDataUpdate();
+patientData02.imc = calculateImc(peso, altura);
+patientData02.resultadoImc = ImcAbove20Years(peso, altura);
 
-console.log(patientData01, patientData02, patientData03);
+console.log(patientData01, patientData02, patientData03, patientData04, patientData05, patientData06);
