1f6aade7b7552795dc9b57c378392133e25e6eb4
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-10-25 03:04:47 -0300

    Exercises the of content finished

---
+++ m-2-dev-front-end/section-03-components-with-state-events-and-forms-with-react/day-1-components-with-state-and-events/3.1-content-exercises/fancy-buttons/src/App.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import Button from './Components/Button';
+
+class App extends Component {
+  render() {
+    return <Button />
+   
+  }
+}
+
+export default App;
