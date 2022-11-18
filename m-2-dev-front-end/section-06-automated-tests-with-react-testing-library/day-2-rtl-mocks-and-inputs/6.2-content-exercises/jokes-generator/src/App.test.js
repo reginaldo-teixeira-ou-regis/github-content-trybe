839ff28a5e928a1981aed8614add9f25a0f693e0
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-11-18 19:30:55 -0300

    Exercise of content of day 6.2 finished

---
+++ m-2-dev-front-end/section-06-automated-tests-with-react-testing-library/day-2-rtl-mocks-and-inputs/6.2-content-exercises/jokes-generator/src/App.test.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+
+afterEach(() => jest.clearAllMocks());
+
+it('fetches a joke', async () => {
+  const joke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+  
+// Primeira forma de mock do fetch
+/* jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke),
+  });
+
+  render(<App />);
+  const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
+  expect(renderedJoke).toBeInTheDocument();
+  expect(global.fetch).toHaveBeenCalledTimes(1);
+  expect(global.fetch).toHaveBeenCalledWith(
+    'https://icanhazdadjoke.com/',
+    { headers: { Accept: 'application/json' } },
+  );
+}); */
+
+// Segunda forma de mock do fetch
+    /* global.fetch = jest.fn(() => Promise.resolve({
+    json: () => Promise.resolve(joke), */
+// Terceira forma de mock do fetch
+    global.fetch = jest.fn(async () => ({
+    json: async () => joke
+  }));
+
+  render(<App />);
+  const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
+  expect(renderedJoke).toBeInTheDocument();
+  expect(global.fetch).toHaveBeenCalledTimes(1);
+  expect(global.fetch).toHaveBeenCalledWith('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
+});
+
+it('fetches a new joke when button is clicked', async () => {
+  const joke1 = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  const joke2 = {
+    id: 'xXSv492wPmb',
+    joke: 'What is red and smells like blue paint? Red paint!',
+    status: 200,
+  };
+
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(joke1),
+  });
+  
+  render(<App />);
+  const newJokeButton = screen.getByRole('button', { name: 'New joke' });
+  
+  expect(await screen.findByText(joke1.joke)).toBeInTheDocument();
+  expect(screen.queryByText(joke2.joke)).not.toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(1);
+  
+  global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(joke2),
+  });
+  userEvent.click(newJokeButton);
+  
+  expect(await screen.findByText(joke2.joke)).toBeInTheDocument()
+  expect(screen.queryByText(joke1.joke)).not.toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(2);
+});
