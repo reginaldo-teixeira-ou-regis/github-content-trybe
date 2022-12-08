db5ef0e78d240ee0a4d073cda465eb1071d59985
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-12-08 01:24:18 -0300

    Exercise of day 7.5 finished

---
+++ m-2-dev-front-end/section-07-state-management-with-redux/day-5-tests-in-react-redux/7.5-exercises-now-in-practice/form-with-redux/src/components/CustomerList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Customer from './Customer';
+
+function CustomerList({ customers, deleteCustomer }) {
+  return (
+    customers.map((customer, i) => (
+      <Customer
+        key={ customer.email }
+        customer={ customer }
+        deleteCustomer={ deleteCustomer }
+        index={ i }
+      />
+    ))
+  );
+}
+
+export default CustomerList;
