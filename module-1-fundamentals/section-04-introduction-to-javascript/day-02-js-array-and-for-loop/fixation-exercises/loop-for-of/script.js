7db6c145f4014dbc3c35bfd663b4a3226d0d517f
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-01-05 21:19:33 -0300

    Fixation exercises day 2(M1-S04)

---
+++ module-1-fundamentals/section-04-introduction-to-javascript/day-02-js-array-and-for-loop/fixation-exercises/loop-for-of/script.js
@@ -0,0 +1,23 @@
+let numeros = [1,2,3,4,5];
+for(let numero of numeros) {
+  console.log(numero);
+}
+console.log(" ");
+
+let word = 'Hello';
+for (let letter of word) {
+    console.log(letter);
+}
+console.log(" ");
+
+let arrOfNumbers = [10, 20, 30];
+for (let sum of arrOfNumbers) {
+  sum += 1;
+  console.log(sum);
+}
+console.log(" ");
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let friends of names) {
+  console.log(friends);
+}
