9ae0fe6ee4b087bc87522ebd00549e5b3671cd09
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-01-06 17:24:00 -0300

    Fixation exercises day 1(M1-S05)

---
+++ module-1-fundamentals/section-05-js-dom-events-and-web-storage/day-01-js-dom-and-selectors/fixation-exercises/getElementsBy-Id-ClassName-TagName/script.js
@@ -0,0 +1,8 @@
+const paragraphs = document.getElementsByClassName("pClass");
+
+for(let index = 0; index < paragraphs.length; index += 1) {
+    paragraphs[0].style.backgroundColor = "blue";
+    paragraphs[0].style.color = "white";
+}
+
+subtitle.style.color = "green";
