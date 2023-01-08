edca0f723d9fdd1b09beae970353dc4be20d0b41
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-01-07 21:48:24 -0300

    Fixation exercises day 1(M1-S08)

---
+++ module-1-fundamentals/section-08-higher-order-functions-of-js-es6/day-01-higher-order-functions-foreach-find-some-and-every/fixation-exercises/basic-everyday-phrases/script.js
@@ -0,0 +1,21 @@
+/* Crie uma função que retorne a string 'Acordando!!';
+Crie outra função que retorne a string 'Bora tomar café!!';
+Crie mais uma função que retorne a string 'Partiu dormir!!';
+Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores. Exemplo: */
+
+const wakeUp = () => 'Acordando!!';
+const drinkingCoffee = () => 'Bora tomar café!!';
+const letsGoToSleep = () => 'Partiu dormir!!';
+
+const doingThings = (callback) => {
+    const result = callback();
+    console.log(result);
+  };
+
+// Ao chamar a função doingThings:
+doingThings(wakeUp); // Acordando!!
+doingThings(drinkingCoffee); // Bota tomar café!!
+doingThings(letsGoToSleep); // Partiu dormir!!
+
+// Ela deve retornar o valor do respectivo parâmetro, neste caso:
+// Acordando!!
