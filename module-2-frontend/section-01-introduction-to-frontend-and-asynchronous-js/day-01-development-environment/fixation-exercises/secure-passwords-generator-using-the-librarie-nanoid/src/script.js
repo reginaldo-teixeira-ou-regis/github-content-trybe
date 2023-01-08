4202e9d47981752d6382412d073ceb858e7c048c
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-01-08 17:54:07 -0300

    Fixation exercises day 1(M2-S01)

---
+++ module-2-frontend/section-01-introduction-to-frontend-and-asynchronous-js/day-01-development-environment/fixation-exercises/secure-passwords-generator-using-the-librarie-nanoid/src/script.js
@@ -0,0 +1,20 @@
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+
+import './style.css';
+
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
+});
+
+// Usar o comando(usando o Vite) abaixo para mostrar na Tela:
+// npx vite --open
