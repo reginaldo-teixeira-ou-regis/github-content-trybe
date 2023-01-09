c8934476fa9953c49c5e4ac08c2bd41867804ce6
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-01-09 00:20:25 -0300

    Exercises day 4(M2-S01)

---
+++ module-2-frontend/section-01-introduction-to-frontend-and-asynchronous-js/day-04-async-await-and-asynchronous-tests/exercises/magic-card-tests-asynchronous-jest/src/__tests__/favorite.test.js
@@ -0,0 +1,34 @@
+const { saveFavoriteMagicCard } = require('../magic');
+const favoriteCards = require('../data/favoriteCards');
+
+const restoreFavoriteCards = () => {
+  /* favoriteCards.pop(); */
+  favoriteCards.splice(4, favoriteCards.length);
+};
+
+jest.setTimeout(10000);
+
+describe('3 - Testes da função saveFavoriteMagicCard', () => {
+  afterEach(restoreFavoriteCards);
+
+  it('3.1 Testa se um novo card é adicionado a cada execução', async () => {
+    expect.assertions(1);
+    await saveFavoriteMagicCard('130553');
+    expect(favoriteCards).toHaveLength(5);
+  });
+
+  it('3.2 Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
+    expect.assertions(1);
+    expect(favoriteCards).toHaveLength(4);
+  });
+});
+
+describe('4 - Testes da função saveFavoriteMagicCard', () => {
+  it(`4.1 Implemente um teste que verifique que o array que você obteve com o map
+  contém todos os quatro nomes iniciais nomes`, async () => {
+    expect.assertions();
+    const barabam = favoriteCards.map((card) => card.name);
+    expect(barabam).toEqual(['Ancestor\'s Chosen',
+    'Angel of Mercy', 'Aven Cloudchaser', 'Ballista Squad']);
+  });
+});
