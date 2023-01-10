74c23b7e852070067f1123e1d99189b617359613
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-01-09 21:08:56 -0300

    Fixation exercises day 2(M2-S04)

---
+++ module-2-frontend/section-04-lifecycle-of-components-and-react-router/day-02-react-router/fixation-exercises/navigate-between-pages-react-router/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import StrictAccess from './components/StrictAccess';
+import Users from './components/Users';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/strict-access">Strict Access</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route
+            path="/users/:id"
+            render={ (props) => <Users { ...props } greetingMessage="Good Morning" /> }
+          />
+          <Route
+            path="/strict-access"
+            render={ () => (
+              <StrictAccess user={ { username: 'joao', password: '1234' } } />
+            ) }
+          />
+          <Route path="/about" component={ About } />
+          <Route exact path="/" component={ Home } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
