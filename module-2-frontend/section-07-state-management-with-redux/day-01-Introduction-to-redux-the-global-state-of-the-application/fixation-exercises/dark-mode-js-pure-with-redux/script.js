3e49cfdadfa222de9d437134e5bd781522508586
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-01-10 04:39:57 -0300

    Fixation exercises day 1(M2-S07)

---
+++ module-2-frontend/section-07-state-management-with-redux/day-01-Introduction-to-redux-the-global-state-of-the-application/fixation-exercises/dark-mode-js-pure-with-redux/script.js
@@ -0,0 +1,48 @@
+const INITIAL_STATE = {
+  theme: "light",
+};
+
+// 3 passos para configurar o Redux
+// 1 - [x] Configuração do Redux
+// 2 - [x] Disparo de Eventos
+// 3 = [x] Me inscrever na loja de eventos
+
+const buttonEl = document.getElementById("light-switch");
+const lampel = document.getElementById("light-bulb");
+const mainApp = document.getElementById("wrapper");
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "changeTheme":
+      return {
+        ...state,
+        theme: state.theme === "light" ? "dark" : "light",
+      };
+    // case "xablau":
+    //   return {
+    //     ...state,
+    //     theme: state.theme === "light" ? "dark" : "light",
+    //   };
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+store.subscribe(() => {
+  const theme = store.getState().theme;
+  mainApp.className = theme;
+  lampel.src = `images/${theme}.png`;
+});
+
+const changeTheme = { type: "changeTheme", payload: "" };
+
+// const wesleyTheme = { type: "xablau", payload: "" };
+
+buttonEl.addEventListener("click", () => {
+  //   theme = theme === "light" ? "dark" : "light"
+  store.dispatch(changeTheme);
+  //     mainApp.className = theme;
+  //     lampel.src = `img/${theme}.png`;
+});
