f445c583140efeb5fb4ba10597a13b1f986706eb
Reginaldo Teixeira <110859136+reginaldo-teixeira-ou-regis@users.noreply.github.com>
2023-01-12 01:54:02 -0300

    Delete m-2-dev-front-end directory

--- m-2-dev-front-end/section-07-state-management-with-redux/day-2-using-redux-in-react/7.2-content-exercises/reading-and-writing-global-state-info-x-men/src/redux/index.js
+++
@@ -1,6 +0,0 @@
-import { legacy_createStore as createStore } from 'redux';
-import mutantReducer from './reducers/mutantReducer';
-
-const store = createStore(mutantReducer);
-
-export default store;
