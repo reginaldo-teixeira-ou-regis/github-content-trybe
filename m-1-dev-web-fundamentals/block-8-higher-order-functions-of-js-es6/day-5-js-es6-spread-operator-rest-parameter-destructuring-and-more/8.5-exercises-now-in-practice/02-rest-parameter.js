e423fffb279cd6fabceb6b767292a3fa5d11e241
Reginaldo Teixeira <110859136+reginaldo-teixeira-ou-regis@users.noreply.github.com>
2023-01-12 01:46:05 -0300

    Delete m-1-dev-web-fundamentals/block-8-higher-order-functions-of-js-es6 directory

--- m-1-dev-web-fundamentals/block-8-higher-order-functions-of-js-es6/day-5-js-es6-spread-operator-rest-parameter-destructuring-and-more/8.5-exercises-now-in-practice/02-rest-parameter.js
+++
@@ -1,9 +0,0 @@
-/* Crie uma função sum que dado um número ilimitado de parâmetros retorna a soma desses parâmetros. Ao chamar a função dessa forma : sum(4,5,6) o número 15 deve ser retornado.
-
-De olho na dica 👀: use o parâmetro rest. */
-
-const sum = (...param) => param.reduce((accumulator, current) => accumulator + current, 0);
-
-console.log(sum(4,5,6));
-console.log(' ');
-console.log(sum(4,5,6,40,70,10,25,35,5));
