a52f6e17585d5a90cd1c44e030b1eeefb334dcfe
Reginaldo Teixeira <110859136+reginaldo-teixeira-ou-regis@users.noreply.github.com>
2023-01-12 01:36:26 -0300

    Delete m-1-dev-web-fundamentals directory

--- m-1-dev-web-fundamentals/block-3-introduction-to-html-and-css/README.md
+++
@@ -1,5 +0,0 @@
-Block 3 Contents:
-[D1]Page structure;
-[D2]First Steps in CSS;
-[D3]Selectors and Positioning;
-[D4]Semantic HTML;
--- m-1-dev-web-fundamentals/block-4-introduction-to-js-and-programming-logic/day-3-js-programming-logic-and-algorithms/4.3-exercises-now-in-practice/challenge-noel-01-sum-from-1-to-50.js
+++
@@ -1,7 +0,0 @@
-let sum = 0;
-
-for (let count = 0; count <= 50; count = count + 1) {
-  sum += count;
-}
-
-console.log("A soma total de 1 a 50 é: " + sum);
