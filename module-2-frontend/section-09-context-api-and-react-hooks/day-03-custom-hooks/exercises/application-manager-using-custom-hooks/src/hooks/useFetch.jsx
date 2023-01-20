d46b5ddc2c683144a191211a65fd468bdb081c9e
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-01-20 01:03:41 -0300

    Exercises day 3(M2-S09)

---
+++ module-2-frontend/section-09-context-api-and-react-hooks/day-03-custom-hooks/exercises/application-manager-using-custom-hooks/src/hooks/useFetch.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export default function useFetch(url) {
+  const [data,  setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const refresh = () => {
+    setLoading(true);
+    fetch(url)
+      .then(result => result.json())
+      .then(data => setData(data.results))
+      .catch(err => setError(err))
+      .finally(setLoading(false))
+  }
+
+  useEffect(() => {
+    refresh()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
+  return { loading, error, data, refresh };
+
+};
