46a1862e3e203e98a02eeee4df039521a40d2804
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-03-14 03:39:48 -0300

    Fixation exercises day 5(M3-S04)

--- module-3-backend/section-04-introduction-to-web-development-with-node-js/day-05-node-js-express-and-mysql/fixation-exercises/trybecash-api-with-express-and-mysql/src/server.js
+++ module-3-backend/section-04-introduction-to-web-development-with-node-js/day-05-node-js-express-and-mysql/fixation-exercises/trybecash-api-with-express-and-mysql/src/server.js
@@ -1,7 +1,8 @@
 const app = require('./app');
 
-const PORT = 3011;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`TrybeCash API is running on port ${PORT}`);
+  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
 });
