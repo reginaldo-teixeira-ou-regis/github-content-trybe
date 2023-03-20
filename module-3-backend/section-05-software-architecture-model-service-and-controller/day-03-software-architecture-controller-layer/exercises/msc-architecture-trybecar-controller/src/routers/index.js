2f27713bc775dcb1a81e55ce2c7b8d339208faf7
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-03-19 22:11:12 -0300

    Exercises day 3(M3-S05)

---
+++ module-3-backend/section-05-software-architecture-model-service-and-controller/day-03-software-architecture-controller-layer/exercises/msc-architecture-trybecar-controller/src/routers/index.js
@@ -0,0 +1,9 @@
+// src/routers/index.js
+
+const passengerRouter = require('./passenger.router');
+const driverRoutes = require('./driver.router');
+
+module.exports = {
+  passengerRouter,
+  driverRoutes,
+};
