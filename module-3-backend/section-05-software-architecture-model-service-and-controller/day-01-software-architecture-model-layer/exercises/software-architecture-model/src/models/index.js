1ae02e59c8816ce516dec862883acf2518b6bdda
Reginaldo Teixeira <110859136+reginaldo-teixeira-ou-regis@users.noreply.github.com>
2023-03-19 22:01:40 -0300

    Delete module-3-backend/section-05-software-architecture-model-service-and-controller/day-01-software-architecture-model-layer/exercises/software-architecture-model directory

--- module-3-backend/section-05-software-architecture-model-service-and-controller/day-01-software-architecture-model-layer/exercises/software-architecture-model/src/models/index.js
+++
@@ -1,9 +0,0 @@
-const passengerModel = require('./passenger.model');
-const travelModel = require('./travel.model');
-const waypointModel = require('./waypoint.model');
-
-module.exports = {
-  passengerModel,
-  travelModel,
-  waypointModel,
-};
