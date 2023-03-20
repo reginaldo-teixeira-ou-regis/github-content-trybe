dd414f400204919113efb374b6ccecce69d37c18
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-03-19 22:06:17 -0300

    Exercises day 2(M3-S05)

---
+++ module-3-backend/section-05-software-architecture-model-service-and-controller/day-02-software-architecture-service-layer/exercises/msc-architecture-trybecar-service/src/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+/* Apagamos a importação dos models que já refatoramos para camada de service! */
+const { travelModel } = require('./models');
+/* Adicionamos a importação dos services */
+const { passengerService, driverService } = require('./services');
+
+const app = express();
+
+app.use(express.json());
+
+const DRIVER_ON_THE_WAY = 2;
+const TRAVEL_IN_PROGRESS = 3;
+const TRAVEL_FINISHED = 4;
+
+app.post('/passengers/:passengerId/request/travel', async (req, res) => {
+  const { passengerId } = req.params;
+  const { startingAddress, endingAddress, waypoints } = req.body;
+
+  const travel = await passengerService.requestTravel(
+    passengerId, 
+    startingAddress, 
+    endingAddress, 
+    waypoints,
+  );
+  
+  res.status(201).json(travel);
+});
+
+app.get('/drivers/open/travels', async (_req, res) => {
+  const result = await driverService.getWaitingDriverTravels();
+  res.status(200).json(result);
+});
+
+app.put('/drivers/:driverId/travels/:travelId/assign', async (req, res) => {
+  const { travelId, driverId } = req.params;
+
+  await travelModel.updateById(travelId, { driverId, travelStatusId: DRIVER_ON_THE_WAY });
+  const travel = await travelModel.findById(travelId);
+
+  res.status(200).json(travel);
+});
+
+app.put('/drivers/:driverId/travels/:travelId/start', async (req, res) => {
+  const { travelId, driverId } = req.params;
+  await travelModel.updateById(travelId, { driverId, travelStatusId: TRAVEL_IN_PROGRESS });
+  const result = await travelModel.findById(travelId);
+  res.status(200).json(result);
+});
+
+app.put('/drivers/:driverId/travels/:travelId/end', async (req, res) => {
+  const { travelId, driverId } = req.params;
+  await travelModel.updateById(travelId, { driverId, travelStatusId: TRAVEL_FINISHED });
+  const result = await travelModel.findById(travelId);
+  res.status(200).json(result);
+});
+
+module.exports = app;
