8caeaa8f14fd3d186a04c1b8c48f190b51c5b9fc
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-04-27 20:41:13 -0300

    Fixation exercises day 1(M3-S09)

--- module-3-backend/section-09-object-oriented-programming-and-solid/day-01-introduction-to-object-orientation/fixation-exercises/oop-general-syntax/src/Tv.ts
+++ module-3-backend/section-09-object-oriented-programming-and-solid/day-01-introduction-to-object-orientation/fixation-exercises/oop-general-syntax/src/Tv.ts
@@ -38,3 +38,11 @@ class Tv {
 // chame o método turnOn para imprimir seus atributos.
 const tv1 = new Tv('LG', 32, '4K', ['HDMI', 'Ethernet', 'Wi-Fi']);
 tv1.turnOn();
+
+// 4 - Pense em três métodos que você utilizou com alguma frequência 
+// ao longo do curso mesmo sem entender como eles funcionavam “nos bastidores”.
+// Cronometre ⏲️ cinco minutos para pensar e siga adiante!
+// console.log;
+// fetch do JS;
+// connection.execute da biblioteca mysql2;
+// app.use do express.
