1e5b1bb13afb23ee55fc1f879685cbb8f809f057
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-04-27 21:26:36 -0300

    Exercises day 1(M3-S09)

---
+++ module-3-backend/section-09-object-oriented-programming-and-solid/day-01-introduction-to-object-orientation/exercises/object-orientation/src/index.ts
@@ -0,0 +1,13 @@
+import Client from './entities/Client';
+import Order from './entities/Order';
+import OrderItem from './entities/OrderItem';
+
+const client = new Client('João');
+
+const sandwich = new OrderItem('Sanduíche Natural', 5.00);
+const juice = new OrderItem('Suco de Abacaxi', 5.00);
+const dessert = new OrderItem('Gelatina de Uva', 2.50);
+
+const order = new Order(client, [sandwich, juice, dessert], 'dinheiro', 0.10);
+
+console.log(order);
