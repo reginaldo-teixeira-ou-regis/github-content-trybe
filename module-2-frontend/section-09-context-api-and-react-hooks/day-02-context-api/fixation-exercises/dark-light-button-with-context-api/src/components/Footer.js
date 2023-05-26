cbe0f421a99254d2a00a679c2e92701e367e045a
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-05-25 23:27:43 -0300

    Delete module-2-frontend/section-09-context-api-and-react-hooks directory

--- module-2-frontend/section-09-context-api-and-react-hooks/day-02-context-api/fixation-exercises/dark-light-button-with-context-api/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import ThemeContext from '../context/ThemeContext';
-
-function Footer() {
-  const theme = useContext(ThemeContext);
-  return <footer className={theme.color}>Tema Atual: {theme.color}</footer>;
-}
-
-export default Footer;
