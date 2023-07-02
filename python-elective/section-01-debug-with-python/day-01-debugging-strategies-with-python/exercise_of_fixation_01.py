a7ff58b2beba902799f0225c81d24eeccb7c316b
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-07-01 21:12:49 -0300

    Fixation Exercises

---
+++ python-elective/section-01-debug-with-python/day-01-debugging-strategies-with-python/exercise_of_fixation_01.py
@@ -0,0 +1,23 @@
+'''
+1. Qual a importância de debugar o código?
+
+R: Debugar o código é importante para encontrar erros e bugs
+e poder corrigi-los. Além disso, esse processo possibilita
+entender melhor o fluxo do código e como ele funciona.
+
+
+2. Quais são as formas mais comuns de debugar código em Python?
+
+R: As formas mais comuns de debugar código em Python são usar
+o print e o debugger, em especial o embutido do VS Code.
+
+
+3. Quais as vantagens de usar debugger do VS Code?
+
+R: As vantagens de usar o debugger do VS Code são:
+* É possível parar a execução do código em um ponto específico.
+* É possível executar o código passo a passo.
+* É possível visualizar o valor das variáveis
+em cada passo da execução do código.
+* É possível executar o código em modo de debug sem precisar alterar o código.
+'''
