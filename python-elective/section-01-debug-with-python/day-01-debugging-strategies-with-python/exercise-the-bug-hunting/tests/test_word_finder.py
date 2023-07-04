4283d0e004aec64b10cf1b06a434239def70bbd7
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-07-04 20:38:10 -0300

    Delete python-elective/section-01-debug-with-python/day-01-debugging-strategies-with-python/exercise-the-bug-hunting directory

--- python-elective/section-01-debug-with-python/day-01-debugging-strategies-with-python/exercise-the-bug-hunting/tests/test_word_finder.py
+++
@@ -1,8 +0,0 @@
-from src.word_finder import lines_with_word_occurrences
-
-
-def test_lines_with_word_occurrences():
-    file_path = "tests/test_data/word_finder.txt"
-    search_word = "python"
-    occurrences = lines_with_word_occurrences(file_path, search_word)
-    assert occurrences == [1, 2, 3, 4, 6, 8]
