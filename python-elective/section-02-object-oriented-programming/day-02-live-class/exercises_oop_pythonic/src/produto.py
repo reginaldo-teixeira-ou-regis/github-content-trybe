d7177a1184df05a86dfa3a6f08c17d1c8a59a5f5
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-07-11 20:33:46 -0300

    Exercises OOP Pythonic

--- python-elective/section-02-object-oriented-programming/day-02-live-class/exercises_oop_pythonic/src/produto.py
+++ python-elective/section-02-object-oriented-programming/day-02-live-class/exercises_oop_pythonic/src/produto.py
@@ -2,19 +2,28 @@ class Produto:
     def __init__(
         self, nome: str, codigo: str, preco: float, quantidade: int
     ) -> None:
-        pass
+        self.__nome = nome
+        self.__codigo = codigo
+        self.__preco = preco
+        self.__quantidade = quantidade
 
     def get_preco(self) -> float:
-        pass
+        return self.__preco
 
     def get_quantidade(self) -> int:
-        pass
+        return self.__quantidade
 
     def atualizar_preco_do_produto(self, novo_preco: float) -> None:
-        pass
+        if novo_preco < 0:
+            raise ValueError("Preço não pode ser negativo")
+        self.__preco = novo_preco
 
     def adicionar_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        if quantidade < 0:
+            raise ValueError("Quantidade não pode ser negativa")
+        self.__quantidade += quantidade
 
     def remover_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+        if quantidade > self.__quantidade:
+            raise ValueError("Quantidade não pode ser maior que o estoque")
+        self.__quantidade -= quantidade
