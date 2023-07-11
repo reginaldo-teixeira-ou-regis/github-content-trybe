7d56f9703226b652d896e0df270b224da2cdf658
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-07-11 17:52:49 -0300

    Fixation Exercises

---
+++ python-elective/section-02-object-oriented-programming/day-01-oop-in-python/exercise_of_fixation_01.py
@@ -0,0 +1,2 @@
+def add_two_numbers(num1: int | float, num2: int | float) -> int | float:
+    return num1 + num2
---
+++ python-elective/section-02-object-oriented-programming/day-01-oop-in-python/exercise_of_fixation_02.py
@@ -0,0 +1,5 @@
+class Pessoa:
+    def __init__(self, nome: str, idade: int, altura: float) -> None:
+        self.nome = nome
+        self.idade = idade
+        self.altura = altura
